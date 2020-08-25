681edbd0b4e4f631f86575a00f0e638b7da023b2
unknown <sacali99@gmail.com>
2020-08-25 16:10:04 -0500

    Realizo pruebas unitarias del método consultarSaldo de la clase cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -12,7 +12,18 @@ public class Cliente {
     }
 
     public double retirar(double cantidad){
-        return 0;
+        if(cantidad <= this.tipoCuenta.Saldo){
+            this.tipoCuenta.Saldo = this.tipoCuenta.Saldo - cantidad;
+            System.out.println("Transación exitosa, su nuevo saldo es $ "+this.tipoCuenta.Saldo);
+            return this.tipoCuenta.Saldo;
+        }else{
+            System.err.println("Saldo insuficiente su saldo actual es de $ "+this.tipoCuenta.Saldo+" Y usted deseea retirar $ "+cantidad);
+            return this.tipoCuenta.Saldo;
+        }
+    }
+    public double consultarSaldo(){
+        System.out.println("su saldo es de $ "+this.tipoCuenta.Saldo);
+        return this.tipoCuenta.Saldo;
     }
 
 }
--- src/ClienteTest.java
+++ src/ClienteTest.java
@@ -7,6 +7,16 @@ public class ClienteTest {
     public void retirar(){
         Cliente juan = new Cliente("Juan",115456972,new Cuenta(127856,50),"Cedula");
         double p = juan.retirar(20.25);
-        assertEquals(29.25,p,0.01);
+        double k = juan.retirar(0.75);
+        assertEquals(29.75,p,0.01);
+        assertEquals(29,k,0.01);
+        double f = juan.consultarSaldo();
+        assertEquals(29,f,0.01);
+    }
+    @Test
+    public void consultarSaldo(){
+        Cliente juan = new Cliente("Juan",115456972,new Cuenta(127856,50),"Cedula");
+        double p = juan.consultarSaldo();
+      assertEquals(50,p,0.01);
     }
 }
